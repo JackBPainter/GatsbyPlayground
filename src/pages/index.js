f07319db66a91aa6086f421ea89413f1b19693aa
Jack Painter <littlesympathy@jacks-mbp.home>
2020-02-19 21:56:22 +0000

    Create simple layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "./components/header"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <Link to="/Contact/">Contact</Link>
+    <Header headerText="Welcome to the Gatbsy Playground" />
+    <p>What a place</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+)
